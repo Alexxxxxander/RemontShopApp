4748e289e203487a16cc0eca3d1026101dba2b9c
user <user@ALEXLAPTOP>
2024-01-17 11:18:07 +0300

    Добавьте файлы проекта.

---
+++ UI/LoginWindow.xaml.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using RemontApp.DB;
+
+namespace RemontApp.UI
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class LoginWindow : Window
+    {
+        public LoginWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnEnter_Click(object sender, RoutedEventArgs e)
+        {
+            string login = TxtBoxLogin.Text;
+            string password = UCPassword.GetPassword();
+            string error = "";
+            if(login != null && login != "" && login != " ")
+            {
+                if (password != null && login != "" && login != " ")
+                {
+                    try
+                    {
+                        if (RemontPracticeEntities.GetContext().Users.Any(x => x.Login == login && x.Password == password))
+                        {
+                            if (RemontPracticeEntities.GetContext().Users.Any(x => x.Login == login && x.Password == password && x.RoleId == 1))
+                            {
+                                MainWindowMenager mainWindowMenager = new MainWindowMenager();
+                                mainWindowMenager.Show();
+                                this.Close();
+                            }
+                            if (RemontPracticeEntities.GetContext().Users.Any(x => x.Login == login && x.Password == password && x.RoleId == 2))
+                            {
+                                MainWindowExecutor mainWindowExecutor = new MainWindowExecutor();
+                                mainWindowExecutor.Show();
+                                this.Close();
+                            }
+                        }
+                        else
+                        {
+                            error += "Пользователь не найден";
+                        }
+                    }
+                    catch(Exception ex)
+                    {
+                        error += ex.Message;
+                    }
+                    
+                }
+                else
+                {
+                    error += "Введите пароль";
+                }
+                
+            }
+            else
+            {
+                error += "Введите логин";
+            }
+            if (error != "")
+            {
+                MessageBox.Show(error, "Ошибка", MessageBoxButton.OK, MessageBoxImage.Information);
+            }
+        }
+    }
+}
---
+++ UI/UserControl/UserControlPasswordBox.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace RemontApp.UI.UserControl
+{
+    /// <summary>
+    /// Логика взаимодействия для UserControlPasswordBox.xaml
+    /// </summary>
+    public partial class UserControlPasswordBox : System.Windows.Controls.UserControl
+    {
+        string password;
+
+        public string GetPassword()
+        {
+            return password;
+        }
+        public UserControlPasswordBox()
+        {
+            InitializeComponent();
+        }
+
+        private void CheckBox_Checked(object sender, RoutedEventArgs e)
+        {
+            TxtBox.Visibility = Visibility.Visible;
+            PswBox.Visibility = Visibility.Hidden;
+            TxtBox.Text = password;
+        }
+
+        private void CheckBox_Unchecked(object sender, RoutedEventArgs e)
+        {
+            TxtBox.Visibility = Visibility.Hidden;
+            PswBox.Visibility = Visibility.Visible;
+            PswBox.Password = password;
+        }
+
+        private void TxtBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            password = TxtBox.Text;
+        }
+
+        private void PswBox_PasswordChanged(object sender, RoutedEventArgs e)
+        {
+            password = PswBox.Password;
+        }
+    }
+}
